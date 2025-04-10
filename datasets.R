2238c9e40551210fdf99a1e3a141ba92b967709d
Lily Beaudion <beaudion@bu.edu>
2025-04-10 14:56:14 -0400

    first commit

---
+++ datasets.R
@@ -0,0 +1,10 @@
+# list of possible datasets for Barbados
+# datasets gathered from WHO:
+# https://data.who.int/countries
+# i chose Barbados
+
+# maternal mortality rates
+maternal <- read.csv('~/Downloads/052_Barbados/AC597B1_3.1.1 - Maternal mortality ratio/052_AC597B1_Dataset_2023-05-11.csv')
+
+# infant mortality rates
+infant <- read.csv("~/Downloads/052_Barbados/A4C49D3_3.2.2- Neonatal mortality rate/052_A4C49D3_Dataset_2024-05-16.csv")
