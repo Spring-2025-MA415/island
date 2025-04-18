6a2a205693028f348293dc0a9e4bdfa817c72819
Lily Beaudion <beaudion@bu.edu>
2025-04-17 20:53:49 -0400

    found more data

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ This repository contains the data I collected for a specific island, Barbados.
 
 I found the shapefile here: https://simplemaps.com/gis/country/bb.
 
-I found all health information regarding Barbados on the WHO website: https://data.who.int/countries/052.
-All of this data is found in the folder `data` with a data dictionary available that is applicable to all datasets.
+## Data Information
+- Health information was found on the WHO website: https://data.who.int/countries/052.
+  - There is a data dictionary available that is applicable to all WHO datasets called `who_data_dictionary.csv`.
+- Demographic information was found on the UN website: https://population.un.org/wpp/downloads?folder=Standard%20Projections&group=CSV%20format
+  - The definitions of the columns are located on this website.
--- datasets.R
+++ datasets.R
@@ -1,14 +1,15 @@
 library(tmap)
 library(dplyr)
 library(sf)
+library(viridis)
+library(paletteer)
 
+# make sure i make good use of shiny
+# data analysis
+# clean code
+# clean repo
 
-# maternal mortality rates
-maternal <- read.csv('./data/AC597B1_3.1.1 - Maternal mortality ratio/052_AC597B1_Dataset_2023-05-11.csv')
-
-# infant mortality rates
-infant <- read.csv("./data/A4C49D3_3.2.2- Neonatal mortality rate/052_A4C49D3_Dataset_2024-05-16.csv")
-
+################# Mapping #################
 # shapefile
 barbados <- st_read("./shapefile/bb.shp")
 barbados <- barbados |> rename(Parish = name,
@@ -18,7 +19,120 @@ barbados <- barbados |> rename(Parish = name,
 barbados <- barbados |> arrange(ID)
 
 # plotting
-tm_shape(barbados) + 
-  tm_polygons() + 
-  tm_graticules(lines=FALSE) +
-  tm_fill("name", palette = "rainbow")
+map <- tm_shape(barbados) + 
+  tm_polygons(fill = "Parish", 
+              fill.scale = tm_scale(values = paletteer_d("ggsci::category20_d3")[1:11]))
+map
+
+
+
+
+
+
+################# Datasets #################
+
+####### WHO Data #######
+# data dictionary
+dict <- read.csv("data/data_dictionary.csv")
+
+# vaccination access
+vaccines <- read.csv("data/Access to affordable medicines and vaccines/dataset.csv")
+
+# tobacco usage
+tobacco <- read.csv("data/Age-standardized prevalence of tobacco use/dataset.csv")
+
+# alcohol usage
+alcohol <- read.csv("data/Alcohol/dataset.csv")
+
+# attended births
+births <- read.csv("data/Births attended by skilled health personnel/dataset.csv")
+
+# diseases
+diseases <- read.csv("data/cardiovascular disease, cancer, diabetes or chronic respiratory disease/dataset.csv")
+
+# clean fuels
+fuels <- read.csv("data/Clean fuels/dataset.csv")
+
+# road traffic deaths
+traffic_deaths <- read.csv("data/Deaths due to road traffic injuries/dataset.csv")
+
+# doctor density and distribution
+doctors <- read.csv("data/Doctor density and distribution/dataset.csv")
+
+# fine particulate matter
+matter <- read.csv("data/Fine particulate matter/dataset.csv")
+
+# government spending on essential services
+gov_spending <- read.csv("data/Government spending essential services/dataset.csv")
+
+# hepatitis b
+hep_b <- read.csv("data/Hepatitis B/dataset.csv")
+
+# HIV infections
+hiv <- read.csv("data/HIV infections/dataset.csv")
+
+# air pollution
+pollution <- read.csv("data/Household and ambient air pollution/dataset.csv")
+
+# hypertension
+hypertension <- read.csv("data/Hypertension/dataset.csv")
+
+# homicide
+homicide <- read.csv("data/Intentional homicide/dataset.csv")
+
+# malnutrition
+malnutrition <- read.csv("data/Malnutrition/dataset.csv")
+
+# maternal mortality rates
+maternal <- read.csv('./data/Maternal mortality ratio/dataset.csv')
+
+# neglected tropical diseases
+trop_diseases <- read.csv("data/Neglected tropical diseases/dataset.csv")
+
+# neonatal mortality rates
+infant <- read.csv("./data/Neonatal mortality rate/dataset.csv")
+
+# nurses and midwives density
+nurse_midwife <- read.csv("data/Nurses and midwives density and distribution/dataset.csv")
+
+# obesity in adults
+obesity_adult <- read.csv("data/Obesity in adults/dataset.csv")
+
+# obesity in children
+obesity_child <- read.csv("data/Obesity in children/dataset.csv")
+
+# pharmacists density
+pharms <- read.csv("data/Pharmacists density and distribution/dataset.csv")
+
+# sanitation and hand-washing
+sanitation <- read.csv("data/Safely managed sanitation & hand-washing/dataset.csv")
+
+# stunting
+stunting <- read.csv("data/Stunting/dataset.csv")
+
+# suicide
+suicide <- read.csv("data/Suicide/dataset.csv")
+
+# tuberculosis
+tb <- read.csv("data/Tuberculosis/dataset.csv")
+
+# under-5 mortality rate
+under5_mortality <- read.csv("data/Under-five mortality rate/dataset.csv")
+
+# unintentional poisoning
+poisoning <- read.csv("data/Unintentional poisoning/dataset.csv")
+
+# unsafe water
+unsafe_water <- read.csv("data/Unsafe water, unsafe sanitation and lack of hygiene/dataset.csv")
+
+# women satisfied with modern methods
+women_methods <- read.csv("data/Women satisfied with modern methods/dataset.csv")
+
+
+
+####### UN Data #######
+# demographics, contains all UN country data
+demographics <- read.csv("data/WPP2024_Demographic_Indicators_Medium.csv.gz")
+# filtering to get all Barbados information
+demographics <- demographics |> filter(Location == "Barbados")
+
